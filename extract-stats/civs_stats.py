0b44f0ce099c73143d05c9d3f154b0352ea6a075
jdduprey <jduprey@uw.edu>
2024-09-17 13:02:52 -0500

    adding matchups, maps data to query, docstring stuff

--- extract-stats/civs_stats.py
+++ extract-stats/civs_stats.py
@@ -168,8 +168,7 @@ def civ_stats_pipeline(start_date, end_date):
                                 ]
                             }, 
                             'then': '1501-1750'
-                        },
-                        {
+                        }, {
                             'case': {
                                 '$gte': [
                                     '$cleanmatchHistory.avg_elo', 1751
@@ -182,32 +181,198 @@ def civ_stats_pipeline(start_date, end_date):
             }
         }
     }, {
-        '$group': {
-            '_id': {
-                'civ_id': '$cleanmatchHistory.civilization_id', 
-                'elo_bin': '$cleanmatchHistory.elo_bin', 
-                'matchDay': '$matchDay'
-            }, 
-            'matchDay': {
-                '$first': '$matchDay'
-            }, 
-            'totalResults': {
-                '$sum': 1
-            }, 
-            'avgDuration': {
-                '$avg': '$matchDuration'
-            }, 
-            'totalWins': {
-                '$sum': {
-                    '$cond': [
-                        {
-                            '$eq': [
-                                '$cleanmatchHistory.outcome', 1
-                            ]
-                        }, 1, 0
-                    ]
+        '$lookup': {
+            'from': 'major_gods', 
+            'localField': 'cleanmatchHistory.opp_civ_id', 
+            'foreignField': 'id', 
+            'as': 'opp_god_info'
+        }
+    }, {
+        '$addFields': {
+            'cleanmatchHistory.opp_god_name': '$opp_god_info.name'
+        }
+    }, {
+        '$unwind': '$cleanmatchHistory.opp_god_name'
+    }, {
+        '$project': {
+            'opp_god_info': 0, 
+            'matchDate': 0
+        }
+    }, {
+        '$facet': {
+            'justCivs': [
+                {
+                    '$group': {
+                        '_id': {
+                            'civ_id': '$cleanmatchHistory.civilization_id', 
+                            'elo_bin': '$cleanmatchHistory.elo_bin', 
+                            'matchDay': '$matchDay'
+                        }, 
+                        'matchDay': {
+                            '$first': '$matchDay'
+                        }, 
+                        'totalResults': {
+                            '$sum': 1
+                        }, 
+                        'avgDuration': {
+                            '$avg': '$matchDuration'
+                        }, 
+                        'totalWins': {
+                            '$sum': {
+                                '$cond': [
+                                    {
+                                        '$eq': [
+                                            '$cleanmatchHistory.outcome', 1
+                                        ]
+                                    }, 1, 0
+                                ]
+                            }
+                        }
+                    }
+                }
+            ], 
+            'oppCivs': [
+                {
+                    '$group': {
+                        '_id': {
+                            'civ_id': '$cleanmatchHistory.civilization_id', 
+                            'opp_civ_id': '$cleanmatchHistory.opp_god_name', 
+                            'elo_bin': '$cleanmatchHistory.elo_bin', 
+                            'matchDay': '$matchDay'
+                        }, 
+                        'matchDay': {
+                            '$first': '$matchDay'
+                        }, 
+                        'totalResults': {
+                            '$sum': 1
+                        }, 
+                        'avgDuration': {
+                            '$avg': '$matchDuration'
+                        }, 
+                        'totalWins': {
+                            '$sum': {
+                                '$cond': [
+                                    {
+                                        '$eq': [
+                                            '$cleanmatchHistory.outcome', 1
+                                        ]
+                                    }, 1, 0
+                                ]
+                            }
+                        }
+                    }
+                }
+            ], 
+            'mapData': [
+                {
+                    '$group': {
+                        '_id': {
+                            'civ_id': '$cleanmatchHistory.civilization_id', 
+                            'elo_bin': '$cleanmatchHistory.elo_bin', 
+                            'map_name': '$mapData.name', 
+                            'matchDay': '$matchDay'
+                        }, 
+                        'matchDay': {
+                            '$first': '$matchDay'
+                        }, 
+                        'totalResults': {
+                            '$sum': 1
+                        }, 
+                        'avgDuration': {
+                            '$avg': '$matchDuration'
+                        }, 
+                        'totalWins': {
+                            '$sum': {
+                                '$cond': [
+                                    {
+                                        '$eq': [
+                                            '$cleanmatchHistory.outcome', 1
+                                        ]
+                                    }, 1, 0
+                                ]
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }, {
+        '$unwind': {
+            'path': '$justCivs'
+        }
+    }, {
+        '$addFields': {
+            'justCivs.mapData': {
+                '$filter': {
+                    'input': '$mapData', 
+                    'as': 'mapDoc', 
+                    'cond': {
+                        '$and': [
+                            {
+                                '$eq': [
+                                    '$$mapDoc._id.civ_id', '$justCivs._id.civ_id'
+                                ]
+                            }, {
+                                '$eq': [
+                                    '$$mapDoc._id.elo_bin', '$justCivs._id.elo_bin'
+                                ]
+                            }, {
+                                '$eq': [
+                                    '$$mapDoc._id.matchDay', '$justCivs._id.matchDay'
+                                ]
+                            }
+                        ]
+                    }
                 }
             }
+        }
+    }, {
+        '$project': {
+            'mapData': 0
+        }
+    }, {
+        '$addFields': {
+            'justCivs.oppData': {
+                '$filter': {
+                    'input': '$oppCivs', 
+                    'as': 'oppDoc', 
+                    'cond': {
+                        '$and': [
+                            {
+                                '$eq': [
+                                    '$$oppDoc._id.civ_id', '$justCivs._id.civ_id'
+                                ]
+                            }, {
+                                '$eq': [
+                                    '$$oppDoc._id.elo_bin', '$justCivs._id.elo_bin'
+                                ]
+                            }, {
+                                '$eq': [
+                                    '$$oppDoc._id.matchDay', '$justCivs._id.matchDay'
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }, {
+        '$replaceRoot': {
+            'newRoot': '$justCivs'
+        }
+    }, {
+        '$project': {
+            '_id': 1, 
+            'matchDay': 1, 
+            'totalResults': 1, 
+            'avgDuration': 1, 
+            'totalWins': 1, 
+            'mapData._id.map_name': 1, 
+            'mapData.totalResults': 1, 
+            'mapData.totalWins': 1, 
+            'oppData._id.opp_civ_id': 1, 
+            'oppData.totalResults': 1, 
+            'oppData.totalWins': 1
         }
     }, {
         '$addFields': {
@@ -216,10 +381,6 @@ def civ_stats_pipeline(start_date, end_date):
                     '$avgDuration', 60
                 ]
             }
-        }
-    }, {
-        '$project': {
-            'avgDuration': 0
         }
     }, {
         '$lookup': {
@@ -237,6 +398,51 @@ def civ_stats_pipeline(start_date, end_date):
     }, {
         '$project': {
             'god_info': 0
+        }
+    }, {
+        '$addFields': {
+            'oppObj': {
+                '$arrayToObject': {
+                    '$map': {
+                        'input': '$oppData', 
+                        'as': 'item', 
+                        'in': {
+                            'k': {
+                                '$toString': '$$item._id.opp_civ_id'
+                            }, 
+                            'v': {
+                                'totalResults': '$$item.totalResults', 
+                                'totalWins': '$$item.totalWins'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, {
+        '$addFields': {
+            'mapObj': {
+                '$arrayToObject': {
+                    '$map': {
+                        'input': '$mapData', 
+                        'as': 'item', 
+                        'in': {
+                            'k': {
+                                '$toString': '$$item._id.map_name'
+                            }, 
+                            'v': {
+                                'totalResults': '$$item.totalResults', 
+                                'totalWins': '$$item.totalWins'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, {
+        '$project': {
+            'mapData': 0, 
+            'oppData': 0
         }
     }, {
         '$addFields': {
@@ -278,15 +484,14 @@ def civ_stats_pipeline(start_date, end_date):
                                 ]
                             }, 
                             'then': 1501
-                        },
-                        {
+                        }, {
                             'case': {
                                 '$eq': [
                                     '$_id.elo_bin', '1751-2000'
                                 ]
                             }, 
                             'then': 1751
-                        },
+                        }
                     ], 
                     'default': None
                 }
@@ -349,7 +554,8 @@ def civ_stats_pipeline(start_date, end_date):
     }, {
         '$project': {
             '_id': 0, 
-            'metaField.matchDay': 0
+            'metaField.matchDay': 0, 
+            'avgDuration': 0
         }
     }
     ]
@@ -366,6 +572,16 @@ def create_daily_stats(target, ingest_custom_range=False, start_date=None, end_d
         only ingest yesterday (in UTC). 
     start_date: (str)  MM/DD/YYYY
     end_date: (str)  MM/DD/YYYY
+
+    Ingesting full time series example: It is Sept 17th, 2024. User wants to ingest all match data 
+    since release. Arguments should be as follows:
+    ingest_custom_range = True
+    start_date = '08/26/2024'
+    end_date = '09/17/2024' 
+    NOTE: The last full day ingested with these params will be 09/16/2024. Then at 2am UTC on 09/18/2024 
+    September 17th will be ingested and the pipeline is off to the races.
+    NOTE: Invoke local lambda test with env_vars as: 
+    `sam local invoke CivsStatsExtractorFunction --env-vars locals.json`
     """
     
     if ingest_custom_range:
@@ -394,12 +610,11 @@ def create_daily_stats(target, ingest_custom_range=False, start_date=None, end_d
 
     return docs
 
-# event structure example?
+# event structure example
 # event = {
-#     "target": "stats_pymongo_test",
 #     "ingest_custom_range": True,
 #     "start_date": '08/26/2024', # one day before aom release = 08/26/2024
-#     "end_date": '09/11/2024',
+#     "end_date": '09/17/2024',
 # }
 
 
